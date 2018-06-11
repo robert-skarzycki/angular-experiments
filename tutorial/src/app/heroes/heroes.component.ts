7e912876eacb7de0869acd6720cabe935f7b9f5f
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-06-11 22:40:13 +0200

    Lista plus style lokalne

--- tutorial/src/app/heroes/heroes.component.ts
+++ tutorial/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,6 +10,7 @@ import { Hero } from '../Hero';
 export class HeroesComponent implements OnInit {
 
   hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
 
   constructor() { }
 
