c59839348b8983c5ebab4c38dab2e79fa3591ca8
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-06-11 22:45:52 +0200

    Krok 4

--- tutorial/src/app/heroes/heroes.component.ts
+++ tutorial/src/app/heroes/heroes.component.ts
@@ -11,10 +11,15 @@ export class HeroesComponent implements OnInit {
 
   hero: Hero = { id: 1, name: 'Windstorm' };
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
