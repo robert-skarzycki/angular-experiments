2919d7b0f9cb3c081c8de6d94e4e65ecbda41433
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-06-13 23:29:09 +0200

    Zapis/edycja via http

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -5,10 +5,16 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+
   private heroesUrl = 'api/heroes';
 
   constructor(
@@ -28,6 +34,19 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id= {id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(tap(_ => this.log(`updated hero id=${hero.id}`),
+        catchError(this.handleError<any>('updateHero'))));
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
