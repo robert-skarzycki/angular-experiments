0972b6b82674cfde500a6a03b8385d0715a85545
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-06-13 22:57:45 +0200

    Krok 8

--- tutorial/src/app/heroes/heroes.component.ts
+++ tutorial/src/app/heroes/heroes.component.ts
@@ -8,19 +8,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = { id: 1, name: 'Windstorm' };
   heroes: Hero[];
-  selectedHero: Hero;
+
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
